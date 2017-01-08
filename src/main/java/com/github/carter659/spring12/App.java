5ad66f8ac615d99a5ba43b946ee5e60e7401cdcf
carter659 <24084182@qq.com>
2017-01-09 02:29:47 +0800

    docker

---
+++ src/main/java/com/github/carter659/spring12/App.java
@@ -0,0 +1,24 @@
+package com.github.carter659.spring12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 博客出处：http://www.cnblogs.com/GoodHelper/
+ *
+ */
+@SpringBootApplication
+@RestController
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "你好,docker";
+	}
+}
